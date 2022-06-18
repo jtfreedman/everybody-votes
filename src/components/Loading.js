90635b717a0761f26f7aed7f8e54fe04c46dd2b6
jtfreedman <babzibles@gmail.com>
2022-06-17 23:17:43 -0400

    added spinner, working on styling of Loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,14 @@
 import classes from './Loading.module.css';
+import Spinner from './ui/Spinner';
 
 export default function Loading() {
   return (
     <div className={classes.loading}>
-      <p>Loading...</p>
+      <div className={classes.fill}/>
+      <div className={classes.text}>
+        <p>Reading data...</p>
+      </div>
+      <Spinner></Spinner>
     </div>
   );
 }
